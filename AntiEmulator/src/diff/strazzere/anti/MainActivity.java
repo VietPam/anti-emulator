074e441253ef316aee00beac7f2bb309840fa6f3
205220153 <20522153@gm.uit.edu.vn>
2026-03-10 16:50:05 +0700

    upgrade enviroment to newest

--- AntiEmulator/src/diff/strazzere/anti/MainActivity.java
+++ AntiEmulator/src/diff/strazzere/anti/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 import android.view.Menu;
 import diff.strazzere.anti.debugger.FindDebugger;
 import diff.strazzere.anti.emulator.FindEmulator;
@@ -12,114 +13,166 @@ import diff.strazzere.anti.taint.FindTaint;
 
 public class MainActivity extends Activity {
 
+    private TextView outputText;
+    private StringBuilder outputBuffer = new StringBuilder();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        outputText = findViewById(R.id.outputText);
+
         new Thread() {
             @Override
             public void run() {
                 super.run();
+
                 isTaintTrackingDetected();
-
                 isMonkeyDetected();
-
                 isDebugged();
-
                 isQEmuEnvDetected();
 
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        outputText.setText(outputBuffer.toString());
+                    }
+                });
             }
         }.start();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     public boolean isQEmuEnvDetected() {
-        log("Checking for QEmu env...");
-        log("hasKnownDeviceId : " + FindEmulator.hasKnownDeviceId(getApplicationContext()));
-        log("hasKnownPhoneNumber : " + FindEmulator.hasKnownPhoneNumber(getApplicationContext()));
-        log("isOperatorNameAndroid : " + FindEmulator.isOperatorNameAndroid(getApplicationContext()));
-        log("hasKnownImsi : " + FindEmulator.hasKnownImsi(getApplicationContext()));
-        log("hasEmulatorBuild : " + FindEmulator.hasEmulatorBuild(getApplicationContext()));
-        log("hasPipes : " + FindEmulator.hasPipes());
-        log("hasQEmuDriver : " + FindEmulator.hasQEmuDrivers());
-        log("hasQEmuFiles : " + FindEmulator.hasQEmuFiles());
-        log("hasGenyFiles : " + FindEmulator.hasGenyFiles());
-        log("hasEmulatorAdb :" + FindEmulator.hasEmulatorAdb());
+
+        log("");
+        log("=== Checking Emulator Environment ===");
+
+        log("    hasKnownDeviceId : " + FindEmulator.hasKnownDeviceId(getApplicationContext()));
+        log("    hasKnownPhoneNumber : " + FindEmulator.hasKnownPhoneNumber(getApplicationContext()));
+        log("    isOperatorNameAndroid : " + FindEmulator.isOperatorNameAndroid(getApplicationContext()));
+        log("    hasKnownImsi : " + FindEmulator.hasKnownImsi(getApplicationContext()));
+        log("    hasEmulatorBuild : " + FindEmulator.hasEmulatorBuild(getApplicationContext()));
+        log("    hasPipes : " + FindEmulator.hasPipes());
+        log("    hasQEmuDriver : " + FindEmulator.hasQEmuDrivers());
+        log("    hasQEmuFiles : " + FindEmulator.hasQEmuFiles());
+        log("    hasGenyFiles : " + FindEmulator.hasGenyFiles());
+        log("    hasEmulatorAdb : " + FindEmulator.hasEmulatorAdb());
+
         for(String abi : Build.SUPPORTED_ABIS) {
             if (abi.equalsIgnoreCase("armeabi-v7a")) {
-                log("hitsQemuBreakpoint : " + FindEmulator.checkQemuBreakpoint());
+                log("    hitsQemuBreakpoint : " + FindEmulator.checkQemuBreakpoint());
             }
         }
-        if (FindEmulator.hasKnownDeviceId(getApplicationContext())
+
+        boolean detected =
+                FindEmulator.hasKnownDeviceId(getApplicationContext())
                         || FindEmulator.hasKnownImsi(getApplicationContext())
                         || FindEmulator.hasEmulatorBuild(getApplicationContext())
-                        || FindEmulator.hasKnownPhoneNumber(getApplicationContext()) || FindEmulator.hasPipes()
-                        || FindEmulator.hasQEmuDrivers() || FindEmulator.hasEmulatorAdb()
+                        || FindEmulator.hasKnownPhoneNumber(getApplicationContext())
+                        || FindEmulator.hasPipes()
+                        || FindEmulator.hasQEmuDrivers()
+                        || FindEmulator.hasEmulatorAdb()
                         || FindEmulator.hasQEmuFiles()
-                        || FindEmulator.hasGenyFiles()) {
-            log("QEmu environment detected.");
-            return true;
+                        || FindEmulator.hasGenyFiles();
+
+        log("");
+        log("    RESULT");
+
+        if (detected) {
+            log("        Emulator environment DETECTED");
         } else {
-            log("QEmu environment not detected.");
-            return false;
+            log("        Emulator environment NOT detected");
         }
+
+        return detected;
     }
 
     public boolean isTaintTrackingDetected() {
-        log("Checking for Taint tracking...");
-        log("hasAppAnalysisPackage : " + FindTaint.hasAppAnalysisPackage(getApplicationContext()));
-        log("hasTaintClass : " + FindTaint.hasTaintClass());
-        log("hasTaintMemberVariables : " + FindTaint.hasTaintMemberVariables());
-        if (FindTaint.hasAppAnalysisPackage(getApplicationContext()) || FindTaint.hasTaintClass()
-                        || FindTaint.hasTaintMemberVariables()) {
-            log("Taint tracking was detected.");
-            return true;
+
+        log("");
+        log("=== Checking Taint Tracking ===");
+
+        log("    hasAppAnalysisPackage : " + FindTaint.hasAppAnalysisPackage(getApplicationContext()));
+        log("    hasTaintClass : " + FindTaint.hasTaintClass());
+        log("    hasTaintMemberVariables : " + FindTaint.hasTaintMemberVariables());
+
+        boolean detected =
+                FindTaint.hasAppAnalysisPackage(getApplicationContext())
+                        || FindTaint.hasTaintClass()
+                        || FindTaint.hasTaintMemberVariables();
+
+        log("");
+        log("    RESULT");
+
+        if (detected) {
+            log("        Taint tracking DETECTED");
         } else {
-            log("Taint tracking was not detected.");
-            return false;
+            log("        Taint tracking NOT detected");
         }
+
+        return detected;
     }
 
     public boolean isMonkeyDetected() {
-        log("Checking for Monkey user...");
-        log("isUserAMonkey : " + FindMonkey.isUserAMonkey());
 
-        if (FindMonkey.isUserAMonkey()) {
-            log("Monkey user was detected.");
-            return true;
+        log("");
+        log("=== Checking Monkey User ===");
+
+        log("    isUserAMonkey : " + FindMonkey.isUserAMonkey());
+
+        boolean detected = FindMonkey.isUserAMonkey();
+
+        log("");
+        log("    RESULT");
+
+        if (detected) {
+            log("        Monkey user DETECTED");
         } else {
-            log("Monkey user was not detected.");
-            return false;
+            log("        Monkey user NOT detected");
         }
+
+        return detected;
     }
 
     public boolean isDebugged() {
-        log("Checking for debuggers...");
+
+        log("");
+        log("=== Checking Debugger ===");
 
         boolean tracer = false;
+
         try {
             tracer = FindDebugger.hasTracerPid();
         } catch (Exception exception) {
             exception.printStackTrace();
         }
 
-        if (FindDebugger.isBeingDebugged() || tracer) {
-            log("Debugger was detected");
-            return true;
+        log("    tracerPid : " + tracer);
+        log("    isBeingDebugged : " + FindDebugger.isBeingDebugged());
+
+        boolean detected = FindDebugger.isBeingDebugged() || tracer;
+
+        log("");
+        log("    RESULT");
+
+        if (detected) {
+            log("        Debugger DETECTED");
         } else {
-            log("No debugger was detected.");
-            return false;
+            log("        No debugger detected");
         }
+
+        return detected;
     }
 
     public void log(String msg) {
         Log.v("AntiEmulator", msg);
+        outputBuffer.append(msg).append("\n");
     }
 }
